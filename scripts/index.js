31956430fe29b10706f21192b9530fcaa96c0e83
Evgeniy Glukhenkiy <evgeniy.glukhenkiy>
2023-12-08 19:34:47 +0300

    код для первой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
-// @todo: Темплейт карточки
+const card = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+function createCard(name, link, deleteCard) {
+    const cardElement = card.querySelector('.card').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+    deleteButton.addEventListener('click', function (event) {
+        deleteCard(event)
+    });
 
-// @todo: Функция удаления карточки
+    return cardElement;
+}
 
-// @todo: Вывести карточки на страницу
+function deleteCard() {
+    const listItem = event.target.closest('.places__item');
+    listItem.remove();
+}
+
+initialCards.forEach(item => placesList.append(createCard(item.name, item.link, deleteCard)));
+/*for(let i=0; i < initialCards.length; i++) {
+    placesList.append(createCard(initialCards[i].name, initialCards[i].link, deleteCard));
+}*/
